f5d375579eacaffe05ea2f333dd8b097364b65e3
mohima-mimino11 <mohimahoque22@gmail.com>
2025-01-12 21:04:07 +0600

    mark as read counter done

--- js/index.js
+++ js/index.js
@@ -8,13 +8,14 @@ const loadPosts = async () =>{
 
 const displayPosts = (posts) =>{
   // console.log(posts);
-  
+  // 1. create divs for all posts
   posts.forEach(post =>{
-    console.log(post);
-    
+    // console.log(post);
+    // get the parent div by ID
     const allPosts = document.getElementById('all-posts');
+    // create new element for the post
     const postDiv = document.createElement('div');
-  
+    // set innerhtml for new element
     postDiv.innerHTML = `
     <div class="mt-12 bg-[#F3F3F5] w-[772px] h-[270px] rounded-3xl ">
         <div class="flex items-center gap-6">
@@ -30,7 +31,7 @@ const displayPosts = (posts) =>{
                 <p class="font-inter text-base">${post?.description}</p>
               </div>
         </div>
-        <div class="mt-[94px] mx-[200px] text-center flex  items-center gap-8 ">
+        <div class="-mt-[208px] mx-[200px] text-center flex  items-center gap-8 ">
             <div>
                <img src="images/tabler-icon-message-2.png" class="w-[28px] h-[28px]">
                ${post?.comment_count}
@@ -44,7 +45,7 @@ const displayPosts = (posts) =>{
                ${post?.posted_time}
             </div>
             <div>
-              <img src="images/email 1.png" class="w-[28px] h-[28px]">
+              <img src="images/email 1.png" class="w-[28px] h-[28px] read-btn" >
             </div>
       </div>
      
@@ -52,11 +53,43 @@ const displayPosts = (posts) =>{
     
     
     `
+    // append new element inside the parent element
     allPosts.appendChild(postDiv);
+    
+     
+      
 
   })
- 
+  // get markAsRead parent elemenet
+  const markAsRead = document.getElementById('read-count');
+  // create new element for the posts that are read
+  const readCountDiv = document.createElement('div');
+  // set classList for the new element
+  readCountDiv.classList=('pt-[24px] px-[24px]');
+  // set innerhtml for new element
+  readCountDiv.innerHTML = `
+      <div class="flex justify-between items-center gap-5">
+          <h5 class="text-xl text-black">Title</h5>
+          <p class="font-inter text-base">Mark as Read(<span id="count-result"></span>)</p>
 
-} 
+      </div>
+    `;
+  markAsRead.appendChild(readCountDiv);
+  
+    const readCountButtons = document.getElementsByClassName('read-btn');
+    const countResult = document.getElementById('count-result');
+    let count = 0;
+    for(let readCountButton of readCountButtons){
+      readCountButton.addEventListener('click', (e) =>{
+        count += 1;
+        // console.log(count);
+        countResult.innerHTML = count;
+      })
+    }
+
+}
+
+
+
 
 loadPosts()
